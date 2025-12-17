0f4329f1ff89ef31b9d08f426842b5eeed60c16a
copyangle <zhhua110@163.com>
2025-12-17 17:13:59 +0800

    撤销修改，等待大版本更新

--- duck_decode_node.py
+++ duck_decode_node.py
@@ -225,22 +225,23 @@ class DuckDecodeNode:
                 arr = np.stack(frames, axis=0).astype(np.float32) / 255.0
                 img_tensor = torch.from_numpy(arr)
             if clip.audio is not None:
-                # try:
-                sr_attr = getattr(clip.audio, "fps", None)
-                sr = int(round(sr_attr)) if sr_attr else 44100
-                audio_np = clip.audio.to_soundarray(fps=sr)
-                # 归一化处理
-                max_val = np.max(np.abs(audio_np))
-                if max_val > 0:
-                    audio_np = audio_np / max_val
+                # todo 这个部分与RH平台存在包冲突
+                try:
+                    sr_attr = getattr(clip.audio, "fps", None)
+                    sr = int(round(sr_attr)) if sr_attr else 44100
+                    audio_np = clip.audio.to_soundarray(fps=sr)
+                    # 归一化处理
+                    max_val = np.max(np.abs(audio_np))
+                    if max_val > 0:
+                        audio_np = audio_np / max_val
 
-                if len(audio_np.shape) == 1:
-                    audio_np = np.expand_dims(audio_np, axis=1)
-                wf = torch.from_numpy(audio_np.T.astype(np.float32)).unsqueeze(0)
-                audio_out = {"waveform": wf, "sample_rate": sr}
-                # except Exception as e:
-                #     audio_out = None
-                #     print(e)
+                    if len(audio_np.shape) == 1:
+                        audio_np = np.expand_dims(audio_np, axis=1)
+                    wf = torch.from_numpy(audio_np.T.astype(np.float32)).unsqueeze(0)
+                    audio_out = {"waveform": wf, "sample_rate": sr}
+                except Exception as e:
+                    audio_out = None
+                    print(e)
             clip.close()
         else:
             img_tensor = torch.zeros((1, 1, 1, 3), dtype=torch.float32)
